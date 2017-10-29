448a3611b992e9fd71cdd6502aadae02e90832cb
Tal Shprecher <tshprecher@gmail.com>
2017-10-29 13:50:28 -0700

    bootstrap edit-generate-test development loop for parsing

---
+++ src/test/java/com/tshprecher/postgres/antlr4/SelectCommandTest.java
@@ -0,0 +1,30 @@
+package com.tshprecher.postgres.antlr4;
+
+import org.antlr.v4.runtime.BaseErrorListener;
+import org.junit.Assert;
+import org.junit.Test;
+import org.antlr.v4.runtime.Recognizer;
+import org.antlr.v4.runtime.RecognitionException;
+import org.antlr.v4.runtime.ANTLRInputStream;
+import org.antlr.v4.runtime.CommonTokenStream;
+
+import java.io.IOException;
+
+public class SelectCommandTest {
+
+    @Test
+    public void testParse() throws IOException {
+        PostgreSQLLexer lexer = new PostgreSQLLexer(new ANTLRInputStream(getClass().getResourceAsStream("/select_1.sql")));
+        PostgreSQLParser parser = new PostgreSQLParser(new CommonTokenStream(lexer));
+
+        parser.addErrorListener(new BaseErrorListener() {
+            @Override
+            public void syntaxError(Recognizer<?, ?> recognizer, Object offendingSymbol, int line, int charPositionInLine, String msg, RecognitionException e)
+                    throws RecognitionException {
+                throw e;
+            }
+                                }
+        );
+        PostgreSQLParser.RootContext context = parser.root();
+    }
+}
